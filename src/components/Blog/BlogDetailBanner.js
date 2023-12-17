798b9d57c15f55a62596498ef702b009323910c0
22sook00 <22sook00@gmail.com>
2023-12-17 17:36:45 +0900

    Hide view counter

--- src/components/Blog/BlogDetailBanner.js
+++ src/components/Blog/BlogDetailBanner.js
@@ -1,18 +1,16 @@
 "use client";
 import React from "react";
 
-import Image from "next/image";
 import { Badge } from "react-ts-sook-ui";
-import GithubSlugger, { slug } from "github-slugger";
+import { slug } from "github-slugger";
 import Link from "next/link";
 import { format, parseISO } from "date-fns";
 
-import ViewCounter from "./ViewCounter";
+//import ViewCounter from "./ViewCounter";
 import { CATEGORY_OBJ } from "@/src/utils/categoryData";
-const slugger = new GithubSlugger();
 
 const BlogDetailBanner = ({ blog, slug: blogSlug }) => {
-  const themeColor = CATEGORY_OBJ[blog.tags[0]].theme;
+  const themeColor = CATEGORY_OBJ[blog.tags[0]]?.theme;
   return (
     <div className="mt-20 text-center relative w-full h-[310px] ">
       {blog && (
@@ -25,7 +23,7 @@ const BlogDetailBanner = ({ blog, slug: blogSlug }) => {
           </div>
           <div className="flex-col-center gap-2  text-gray text-xs">
             <time>{format(parseISO(blog.publishedAt), "LLLL d, yyyy")}</time>
-            <ViewCounter slug={blogSlug} />
+            {/*<ViewCounter slug={blogSlug} />*/}
           </div>
         </>
       )}
